9f1fc49229bb0678ad1eb61f74e5ccb7667bfb5b
Jonan Scheffler <jonan.scheffler@g5platform.com>
2011-01-13 11:17:08 -0800

    Working on pulling Google data

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ActiveRecord::Base
+
+  def get_google_data(isbn)
+    Net::HTTP.get_print "http://books.google.com/books/feeds/volumes?q=isbn:#{isbn}"
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Floatingbooks::Application.initialize!
+
+# Require Net::http so we can request Google data
+require 'net/http'
